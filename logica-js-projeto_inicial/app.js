b94ec811617ba4e42d5a5a08e37e40c47be09b75
Marcos <daveramarcos17@gmail.com>
2024-07-07 12:53:20 -0300

    first commit

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,37 @@
+alert("Boas vindas ao jogo do número secreto");
+let nmrMult = 10;
+let nmrSecreto = parseInt(Math.random() * nmrMult + 1);
+console.log(nmrSecreto);
+let chute;
+let tentativas = 1;
+
+
+//loop
+while (chute != nmrSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${nmrMult}`);
+
+    if (chute == nmrSecreto) {
+        break;
+    }
+    else {
+        if (chute > nmrSecreto) {
+            alert(`Quase lá, o número secreto é menor que ${chute} `);
+        }
+        else {
+            alert(`Quase lá, o número secreto é maior que ${chute} `);
+        }
+        tentativas++;
+
+    }
+}
+
+let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa";
+alert(`Você acertou, o número secreto era ${nmrSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+/*
+if (tentativas > 1) {
+    alert(`Você acertou, o número secreto era ${nmrSecreto} com ${tentativas} tentativas.`);
+}
+else{
+    alert(`Você acertou, o número secreto era ${nmrSecreto} com ${tentativas} tentativa.`);
+}*/
